ecf7c2061a167f87c322c168eeacd026338bda45
Dmitry <zdmitrynsk@gmail.com>
2024-03-22 07:10:58 +0700

    Убрал асинхронную отправку сообщения в рокет чат

--- lib/redmine_rocketchat/listener.rb
+++ lib/redmine_rocketchat/listener.rb
@@ -166,7 +166,7 @@ class RocketchatListener < Redmine::Hook::Listener
         client.ssl_config.cert_store.set_default_paths
         client.ssl_config.ssl_version = :auto
         client.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        client.post_async url, {:payload => params.to_json}
+        client.post url, {:payload => params.to_json}
       rescue Exception => e
         Rails.logger.warn("cannot connect to #{url}")
         Rails.logger.warn(e)
